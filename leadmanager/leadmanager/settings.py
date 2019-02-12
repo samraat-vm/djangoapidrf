aaf4b3f379ed51e3c1355fcdb702b56ca972bc82
Vishal Mishra <vmdevelops@gmail.com>
2019-02-12 21:38:00 +0530

    This is sample REST API using DRF

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&5w8#a0)^htsi1q$*@*mz^v#h05a37so7ecnf@@exnlgg8j6(c'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
